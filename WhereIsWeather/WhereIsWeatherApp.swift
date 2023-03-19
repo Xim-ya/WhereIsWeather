d70a71a88f3a6c3a4cc68522a4d0bcb0c3e141f0
XimYa <hj_lee_com@naver.com>
2023-03-19 16:00:55 +0900

    Initial Commit

---
+++ WhereIsWeather/WhereIsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsWeatherApp.swift
+//  WhereIsWeather
+//
+//  Created by 심야 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
